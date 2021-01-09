45a370e8eea3f57face958258b3268c63cfc6785
ajrinasywa <ajrinanw@student.uns.ac.id>
2021-01-09 17:07:42 +0700

    Create offline.js

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1610186266,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/57a20434622943a9aba539c0ebd93bf-sheet0.png",
+		"images/idle__-sheet0.png",
+		"images/idle__-sheet1.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/sprite-sheet3.png",
+		"images/spikes_-sheet0.png",
+		"images/blade_-sheet0.png",
+		"images/long_chain_piece-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
